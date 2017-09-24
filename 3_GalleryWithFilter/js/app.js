c7ac943224e6c37d1fddd1399afcf7a6d6b3dda7
Jakub Strebeyko <strebeyko@gmx.com>
2017-09-24 18:50:57 +0200

    Gallery with filter: basic functionality & styling

--- 3_GalleryWithFilter/js/app.js
+++ 3_GalleryWithFilter/js/app.js
@@ -1,4 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("EEEEEJJJJJ");
 
-})
+//creating  DOM variables
+    const pictures = document.querySelectorAll("div img");
+    const show = document.querySelector("#showButton");
+    const hide = document.querySelector("#hideButton");
+    const input = document.querySelector("#tagInput");
+//adding events
+        show.addEventListener("click", () => {
+            let text=input.value;
+            for (i=0; i<pictures.length; i++) {
+                if (text != "" && pictures[i].dataset.tag.indexOf(text)>=0){
+                    pictures[i].classList.remove("invisible");
+                }
+            }
+        });
+        hide.addEventListener("click", () => {
+            let text=input.value;
+            for (i=0; i<pictures.length; i++) {
+                if (text != "" && pictures[i].dataset.tag.indexOf(text)>=0){
+                    pictures[i].classList.add("invisible");
+                }
+            }
+        });
+//TODO:create functionality based on tag-buttons
+
+// const spans = document.querySelectorAll("span");
+//
+//     for (i=0; i<spans.length; i++) {
+//         spans[i].addEventListener("click", () => {
+//             let text=spans[i].innerText
+//     }}
+});
