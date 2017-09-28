9cf14ffe49bc9957193f0f6bf2d1ab289a5114e6
Jakub Strebeyko <strebeyko@gmx.com>
2017-09-28 14:25:40 +0200

    Tooltips' tips rewritten to be a CSS3 ::before feature and not a separate JS object

--- 5_Tooltips/js/app.js
+++ 5_Tooltips/js/app.js
@@ -8,29 +8,20 @@ console.log(tooltipArr);
 
 const tooltip = document.createElement("span");
 
-const tip = document.createElement("div");
-
-
 for (let i = 0; i < tooltipArr.length; i++) {
  tooltipArr[i].addEventListener("mouseover", (e) => {
      tooltip.classList.add("tooltipText");
      tooltip.innerHTML = e.target.dataset.text;
-     tip.classList.add("tip");
      tooltipArr[i].appendChild(tooltip);
-     tooltipArr[i].appendChild(tip)
      tooltip.offsetLeft = tooltip.offsetLeft;
      tooltip.classList.toggle('show')
-     tip.offsetLeft = tip.offsetLeft;
-     tip.classList.toggle('show')
 
 ;
 
  })
  tooltipArr[i].addEventListener("mouseout", () => {
      tooltipArr[i].removeChild(tooltip);
-     tooltipArr[i].removeChild(tip);
      tooltip.classList.toggle("show")
-     tip.classList.toggle("show")
 
      })
 }
