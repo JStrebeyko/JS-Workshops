9035081469c2e142a9c60a6e92e754ac4f67c580
Jakub Strebeyko <strebeyko@gmx.com>
2017-09-29 18:30:19 +0200

    Counter does work, but not entirely as desired.

--- 6_ToDoList/js/app.js
+++ 6_ToDoList/js/app.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const toDoList = document.querySelector("#toDoList");
+    console.warn(toDoList);
+
     const taskInput = document.querySelector("#taskInput");
     console.log("Taskbutton: ",taskInput);
 
@@ -12,30 +15,53 @@ document.addEventListener('DOMContentLoaded', () => {
     const removeFinishedTasksButton = document.querySelector("#removeFinishedTasksButton");
     console.log("removeFinishedTasksButton: ",removeFinishedTasksButton);
 
-    // add new task functionality:
+
+    let numberOfTasks = 0;
+    console.log(numberOfTasks);
+
+
+         // add new task functionality:
+         let newTask = document.createElement("li");
+         let newTaskTitle = document.createElement("h1");
+         let newTaskDeleteButton = document.createElement("button");
+         newTaskDeleteButton.innerHTML = "Delete";
+         newTaskDeleteButton.addEventListener("click", () => {
+             numberOfTasks--;
+             newTask.parentElement.removeChild(newTask);
+             console.log();
+         });
+
+         let newTaskCompleteButton = document.createElement("button");
+         newTaskCompleteButton.innerHTML = "Completed";
+         newTaskCompleteButton.addEventListener("click", () => {
+             event.target.parentElement.classList.toggle("complete");
+             if (event.target.parentElement.classList.contains("complete")) {
+                 numberOfTasks--;
+             } else {
+                 numberOfTasks++;
+             }
+             console.log(numberOfTasks);
+
+         });
+
 
     addTaskButton.addEventListener("click", () => {
         if (taskInput.value.length < 5 || taskInput.value.length > 100 ) {
              console.warn("The task description must fit between five and hundret signs!");
          } else {
+             numberOfTasks++;
+             console.log(numberOfTasks)
 
-            let newTask = document.createElement("li");
-
-            let newTaskTitle = document.createElement("h1");
-            newTaskTitle.innerHTML = taskInput.value;
-
-            let newTaskDeleteButton = document.createElement("button");
-            newTaskDeleteButton.innerHTML = "Delete";
-            newTaskDeleteButton.addEventListener("click", () => {
-                newTask.parentElement.removeChild(newTask);
-            });
+             newTaskTitle.innerHTML = taskInput.value;
 
 
-            let newTaskCompleteButton = document.createElement("button");
-            newTaskCompleteButton.innerHTML = "Completed";
-            newTaskCompleteButton.addEventListener("click", () => {
-                event.target.parentElement.classList.toggle("complete")
-            });
+
+
+            let counter = document.createElement("span");
+            counter.classList.add("counter");
+            toDoList.appendChild(counter);
+            counter.innerText = numberOfTasks;
+
 
             newTask.appendChild(newTaskTitle);
             newTask.appendChild(newTaskDeleteButton);
@@ -47,29 +73,20 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    let counter = document.createElement("span");
-    let numberOfUndone = 0;
-    counter.classList.add("counter")
-    counter.innerHTML = numberOfUndone;
-    function countUndoneTasks() {
-        if (!element.classList.contains("complete")) {
-            count++;
-        }
-    }
-    numberOfUndone = [...document.querySelectorAll("li")].forEach(countUndoneTasks)
-
 
 //var removeCompletedArr = [...document.querySelectorAll("li")];
-removeFinishedTasksButton.addEventListener("click", () => {
+    removeFinishedTasksButton.addEventListener("click", () => {
 
-    let removeCompletedArr = [...document.querySelectorAll("li")]
-    function deleteAllCompleted (element) {
-        if (element.classList.contains("complete")) {
-            element.parentElement.removeChild(element);
+        let removeCompletedArr = [...document.querySelectorAll("li")]
+        function deleteAllCompleted (element) {
+            if (element.classList.contains("complete")) {
+                element.parentElement.removeChild(element);
+                numberOfTasks--;
+
+            }
         }
-    }
-        removeCompletedArr.forEach(deleteAllCompleted);
-});
+            removeCompletedArr.forEach(deleteAllCompleted);
+    });
 
 
 });
